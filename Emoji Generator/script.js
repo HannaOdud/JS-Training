8fdcc48f466647f15d25d9942cf24a2d738d8a8c
HannaOdud <annodud@gmail.com>
2025-12-31 00:57:35 +0000

    js done

--- Emoji Generator/script.js
+++ Emoji Generator/script.js
@@ -8,19 +8,82 @@ async function setup(){
     const random = document.getElementById("random");
     //click on button "happy"
     happy.addEventListener("click", () => {
+        const happyArray = emojiDate.Happy;
+        const index = Math.floor(Math.random()* happyArray.length);
+        const element = happyArray[index];
+        const emoji = element.emoji;
+        const name = element.name;
 
+        const pEmoji = document.createElement("p");
+        pEmoji.textContent = emoji;
+        const pName = document.createElement("p");
+        pName.textContent = name;
+        emojiContainer.innerHTML = "";
+        emojiContainer.appendChild(pEmoji);
+        emojiContainer.appendChild(pName);
     })
     sad.addEventListener("click", () => {
+        const sadArray = emojiDate.Sad;
+        const index = Math.floor(Math.random()* sadArray.length);
+        const element = sadArray[index];
+        const emoji = element.emoji;
+        const name = element.name;
 
+        const pEmoji = document.createElement("p");
+        pEmoji.textContent = emoji;
+        const pName = document.createElement("p");
+        pName.textContent = name;
+        emojiContainer.innerHTML = "";
+        emojiContainer.appendChild(pEmoji);
+        emojiContainer.appendChild(pName);
     })
     angry.addEventListener("click", () => {
+        const angryArray = emojiDate.Angry;
+        const index = Math.floor(Math.random()* angryArray.length);
+        const element = angryArray[index];
+        const emoji = element.emoji;
+        const name = element.name;
 
+        const pEmoji = document.createElement("p");
+        pEmoji.textContent = emoji;
+        const pName = document.createElement("p");
+        pName.textContent = name;
+        emojiContainer.innerHTML = "";
+        emojiContainer.appendChild(pEmoji);
+        emojiContainer.appendChild(pName);
     })
     excited.addEventListener("click", () => {
+        const excitedArray = emojiDate.Excited;
+        const index = Math.floor(Math.random()* excitedArray.length);
+        const element = excitedArray[index];
+        const emoji = element.emoji;
+        const name = element.name;
 
+        const pEmoji = document.createElement("p");
+        pEmoji.textContent = emoji;
+        const pName = document.createElement("p");
+        pName.textContent = name;
+        emojiContainer.innerHTML = "";
+        emojiContainer.appendChild(pEmoji);
+        emojiContainer.appendChild(pName);
     })
     random.addEventListener("click", () => {
+        const arrays = [emojiDate.Excited,emojiDate.Sad, emojiDate.Angry,emojiDate.Happy ];
+        const arrayIndex = Math.floor(Math.random()* arrays.length);
+        const randomArray = arrays[arrayIndex];
 
+        const index = Math.floor(Math.random()* randomArray.length);
+        const element = randomArray[index];
+        const emoji = element.emoji;
+        const name = element.name;
+
+        const pEmoji = document.createElement("p");
+        pEmoji.textContent = emoji;
+        const pName = document.createElement("p");
+        pName.textContent = name;
+        emojiContainer.innerHTML = "";
+        emojiContainer.appendChild(pEmoji);
+        emojiContainer.appendChild(pName);
     })
 
 
