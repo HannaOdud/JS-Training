cc07b75bd941c64e86baa52064f12efb9df9b37d
HannaOdud <annodud@gmail.com>
2025-12-31 00:05:32 +0000

    fetch

--- Emoji Generator/script.js
+++ Emoji Generator/script.js
@@ -1,5 +1,44 @@
-function setup(){
-    
+async function setup(){
+    const emojiDate = await fetchEmojis();
+    const emojiContainer = document.getElementById("emojiContainer");
+    const happy = document.getElementById("happy");
+    const sad = document.getElementById("sad");
+    const angry = document.getElementById("angry");
+    const excited = document.getElementById("excited");
+    const random = document.getElementById("random");
+    //click on button "happy"
+    happy.addEventListener("click", () => {
+
+    })
+    sad.addEventListener("click", () => {
+
+    })
+    angry.addEventListener("click", () => {
+
+    })
+    excited.addEventListener("click", () => {
+
+    })
+    random.addEventListener("click", () => {
+
+    })
+
+
+}
+
+async function fetchEmojis() {
+  const url = "Emoji.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 
