ca7599a6f7f2ac91286bbbd819f49cc28bc343a0
HannaOdud <annodud@gmail.com>
2026-01-01 20:26:23 +0000

    js done

--- Simple Counter/script2.js
+++ Simple Counter/script2.js
@@ -5,23 +5,23 @@ function setup(){
     const dec_clickDom = document.getElementById("dec_click");
     const inc_clickDom = document.getElementById("inc_click");
 
+    let number = 0;
+    let dec_click = 0;
+    let inc_click = 0;
+
     dec.addEventListener("click", () =>{
         //change h1
-        let number = Number(valueDom.textContent);
         number--;
         valueDom.textContent = number;
         //change p
-        let dec_click = Number(dec_clickDom.textContent);
         dec_click++;
         dec_clickDom.textContent = dec_click;
     })
       inc.addEventListener("click", () =>{
         //change h1
-        let number = Number(valueDom.textContent);
         number++;
         valueDom.textContent = number;
         //change p
-        let inc_click = Number(inc_clickDom.textContent);
         inc_click++;
         inc_clickDom.textContent = inc_click;
     })
